40fb80e4371a3b22ba13c7e91bbc2e9142770730
Krystian <>
2024-09-23 08:23:21 +0200

    rollup build, not working

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
-import thyseusPlugin from 'thyseus-plugin';
+//import thyseusPlugin from 'thyseus-plugin';
+import { thyseus } from "@thyseus/rollup-plugin-thyseus";
 import typescript from 'rollup-plugin-typescript2';
  
 export default {
-	// ...
+	input: 'src/index.tsx',
+	output: {
+	   file: 'public/bundle.js',
+	   format: 'iife'
+	},
 	plugins: [
-		thyseusPlugin(),
+		thyseus(),
 		typescript(),
 	],
 };
